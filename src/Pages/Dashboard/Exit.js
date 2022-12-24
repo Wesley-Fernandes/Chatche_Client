66c28c8eaeb48a590bdae9e414b592d35485ca55
Wesley Fernandes <wesleyintelligence@protonmail.com>
2022-12-23 21:29:03 -0300

    removendo arquivo exit de dashboard

--- src/Pages/Dashboard/Exit.js
+++
@@ -1,8 +0,0 @@
-import { signOut } from "firebase/auth";
-import { Authentify } from "../../Firebase/firebase";
-
-export default function Exit(){
-    signOut(Authentify).then((response)=>{
-        console.log(response);
-    })
-}
